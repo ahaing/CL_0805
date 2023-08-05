33a00fc6108016bb19a20893a45a3957f0ed9555
ahaing <ahahaing@gmail.com>
2023-08-05 05:41:50 +0000

    randn

--- Lec004.py
+++ Lec004.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 
-st.slider("互動改變bar三角函式",min_value=0,max_value=10)
+variable = st.slider("加入亂數互動改變bar三角函式",min_value=0,max_value=10)
 t = np.arange(0.,1.0,0.05)
-y1 = np.sin(2 * np.pi * t)
-y2 = np.cos(2 * np.pi * t)
+y1 = np.sin(np.random.randn()* variable * np.pi * t)
+y2 = np.cos(np.random.randn()* variable * np.pi * t)
 figure1 = plt.figure(figsize=(8,4))
 axes1 = figure1.add_subplot()
 axes1.plot(y1)
