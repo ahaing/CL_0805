93235a3277325eec372fb7e20b18b31a3965f3ef
ahaing <ahahaing@gmail.com>
2023-08-05 05:20:50 +0000

    bar

--- Lec004.py
+++ Lec004.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 
-st.slider("三角函式",min_value=0,max_value=10)
+st.slider("互動改變bar三角函式",min_value=0,max_value=10)
 t = np.arange(0.,1.0,0.05)
 y1 = np.sin(2 * np.pi * t)
 y2 = np.cos(2 * np.pi * t)
